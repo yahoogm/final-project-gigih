9c989dc8cb039a39d2b12b0e2d2d4b1b3ba10f49
yahoogm <yaho.garfield@gmail.com>
2023-08-11 17:44:42 +0700

    Add wrapper redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux/es/exports';
+import { Store } from './config/redux/store/store.tsx';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persiststore = persistStore(Store);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ChakraProvider>
-    <App />
+    <Provider store={Store}>
+      <PersistGate loading={null} persistor={persiststore}>
+        <App />
+      </PersistGate>
+    </Provider>
   </ChakraProvider>
 );
