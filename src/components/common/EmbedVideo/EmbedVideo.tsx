e9c261d21fcd967bb4f47d323ef97afd0735718e
yahoogm <yaho.garfield@gmail.com>
2023-08-11 18:04:32 +0700

    Create component for embed video & card video

---
+++ src/components/common/EmbedVideo/EmbedVideo.tsx
@@ -0,0 +1,15 @@
+const EmbedVideo = () => {
+  return (
+    <iframe
+      width="768"
+      height="500"
+      src="https://www.youtube.com/embed/SkV_plRkVzo"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowFullScreen
+    ></iframe>
+  );
+};
+
+export default EmbedVideo;
