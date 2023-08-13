b4e3aba2378291dee39e659b5ccf093849ee1b12
yahoogm <yaho.garfield@gmail.com>
2023-08-13 18:02:38 +0700

    Change style card

--- src/components/common/CardVideo/CardVideo.tsx
+++ src/components/common/CardVideo/CardVideo.tsx
@@ -5,9 +5,6 @@ import {
   Stack,
   Heading,
   Text,
-  Divider,
-  CardFooter,
-  Button,
   Link as ChakraLink,
 } from '@chakra-ui/react';
 
@@ -34,49 +31,51 @@ const CardVideo = ({
   return (
     <>
       {price ? (
-        <Card maxW="80">
-          <CardBody>
-            <Image
-              src={imageThumbnail}
-              alt="Green double couch with wooden legs"
-              borderRadius="lg"
-            />
-            <Stack mt="6" spacing="3">
-              <Heading size="md">{title}</Heading>
-              <Text>{authorName}</Text>
-            </Stack>
-          </CardBody>
-          <Divider />
-          <CardFooter>
-            <Button variant="solid" colorScheme="blue">
-              <ChakraLink href={linkProduct} isExternal>
-                Detail
-              </ChakraLink>
-            </Button>
-          </CardFooter>
+        <Card maxW="80" bgColor={'gray.200'}>
+          <ChakraLink
+            href={linkProduct}
+            isExternal
+            sx={{
+              textDecoration: 'none',
+              '&:hover': { textDecoration: 'none' },
+            }}
+          >
+            <CardBody>
+              <Image
+                src={imageThumbnail}
+                alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="sm">{title}</Heading>
+                <Text>{authorName}</Text>
+                <Text color="blue.600" fontSize="md">
+                  {`Rp. ${price}`}
+                </Text>
+              </Stack>
+            </CardBody>
+          </ChakraLink>
         </Card>
       ) : (
-        <Card maxW="sm">
-          <CardBody>
-            <Image
-              src={imageThumbnail}
-              alt="Green double couch with wooden legs"
-              borderRadius="lg"
-            />
-            <Stack mt="6" spacing="3">
-              <Heading size="md">{title}</Heading>
-              <Text>{authorName}</Text>
-            </Stack>
-          </CardBody>
-          <Divider />
-          <CardFooter>
-            <Button variant="solid" colorScheme="blue">
-              <ChakraLink as={ReactRouterLink} to={`/detail-video/${videoId}`}>
-                Detail
-              </ChakraLink>
-            </Button>
-          </CardFooter>
-        </Card>
+        <ChakraLink
+          as={ReactRouterLink}
+          to={`/detail-video/${videoId}`}
+          sx={{ textDecoration: 'none', '&:hover': { textDecoration: 'none' } }}
+        >
+          <Card maxW="sm" bgColor={'gray.200'} height={400}>
+            <CardBody>
+              <Image
+                src={imageThumbnail}
+                alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">{title}</Heading>
+                <Text>{authorName}</Text>
+              </Stack>
+            </CardBody>
+          </Card>
+        </ChakraLink>
       )}
     </>
   );
