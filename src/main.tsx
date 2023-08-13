9ca1bc742d57ea9ec232a7f6031998fb2e784e4c
yahoogm <yaho.garfield@gmail.com>
2023-08-13 17:59:14 +0700

    Add extend theme for body, heading, and mono

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { Provider } from 'react-redux/es/exports';
 import { Store } from './config/redux/store/store.tsx';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const persiststore = persistStore(Store);
+const theme = extendTheme({
+  fonts: {
+    body: 'Menlo, monospace',
+    heading: 'Menlo, monospace',
+    mono: 'Menlo, monospace',
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <Provider store={Store}>
       <PersistGate loading={null} persistor={persiststore}>
         <App />
