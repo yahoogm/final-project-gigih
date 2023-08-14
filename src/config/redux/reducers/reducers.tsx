07a85768197e58933fb062a0cab87e591db84e97
yahoogm <yaho.garfield@gmail.com>
2023-08-14 22:05:07 +0700

    Remove redux files

--- src/config/redux/reducers/reducers.tsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import videoReducers from '../videos/videosSlice';
-
-export const Reducers = combineReducers({
-  videos: videoReducers,
-});
-
-export type RootState = ReturnType<typeof Reducers>;
