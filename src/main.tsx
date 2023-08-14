1060aedb744f147376542106cc07c3782d2aab9d
yahoogm <yaho.garfield@gmail.com>
2023-08-14 22:04:56 +0700

    Remove redux wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import { Provider } from 'react-redux/es/exports';
-import { Store } from './config/redux/store/store.tsx';
-import { persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
 
-const persiststore = persistStore(Store);
 const theme = extendTheme({
   fonts: {
     body: 'Menlo, monospace',
@@ -17,10 +12,6 @@ const theme = extendTheme({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ChakraProvider theme={theme}>
-    <Provider store={Store}>
-      <PersistGate loading={null} persistor={persiststore}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <App />
   </ChakraProvider>
 );
